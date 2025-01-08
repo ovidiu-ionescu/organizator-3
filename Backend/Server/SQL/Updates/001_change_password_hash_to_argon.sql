f122e2c7b801cef08ec9d74d61ad5815b971c511
Ovidiu Ionescu <ovidiu@ionescu.net>
2025-01-08 19:36:09 +0100

    Added the script to set the row level security on the users table

--- Backend/Server/SQL/Updates/001_change_password_hash_to_argon.sql
+++ Backend/Server/SQL/Updates/001_change_password_hash_to_argon.sql
@@ -12,3 +12,25 @@ CREATE VIEW user_view AS
 
 -- Add a new column for the new password hash.
 ALTER TABLE users ADD COLUMN password_hash text;
+
+---------------------------------------------------
+-- Set the row level security policy for the users table.
+
+-- we use the table owner as the user so security is enforced
+ALTER TABLE users FORCE ROW LEVEL SECURITY;
+
+-- everybody can select
+DROP POLICY IF EXISTS select_policy ON users;
+CREATE POLICY select_policy on users
+FOR SELECT USING(true)
+
+DROP POLICY IF EXISTS update_policy ON users;
+CREATE POLICY update_policy ON users
+FOR UPDATE
+USING (current_setting('organizator.current_user')::int in (id, 1));
+
+DROP POLICY IF EXISTS insert_policy ON users;
+CREATE POLICY insert_policy ON users
+FOR INSERT
+USING (current_setting('organizator.current_user')::int = 1);
+`
